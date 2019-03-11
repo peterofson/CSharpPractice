f8fb0177753945db2f703defe829179954bd9664
David Peterson <44169089+peterofson@users.noreply.github.com>
2019-03-11 15:46:09 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,16 +10,11 @@ namespace myApp
             Double final_yards; 
             Double feetz; 
             Double tst ;
-
-            if (dist < 5280){
-                fnl_miles = 0;
-            }
+            if (dist < 5280){fnl_miles = 0;}
             else { fnl_miles = dist/5280; }
-             
             tst = miles + Math.Truncate(fnl_miles);
             final_yards = Math.Truncate((dist%5280)/3);
             feetz = Convert.ToInt16((dist%5280)%3);
-
             Console.WriteLine("Miles: " + tst);
             Console.WriteLine("Yards: " + Math.Truncate(final_yards));
             Console.WriteLine("Feet : " + feetz);
@@ -29,14 +24,10 @@ namespace myApp
             Double fnl_miles;
             Double final_yards; 
             Double tst;
-
-            if (dist < 1760 ){
-                fnl_miles = 0;
-            }
+            if (dist < 1760 ){fnl_miles = 0;}
             else { fnl_miles = dist/1760;}
             tst = miles + Math.Truncate(fnl_miles);
             final_yards = Math.Truncate(dist%1760);
-
             Console.WriteLine("Miles: " + tst);
             Console.WriteLine("Yards: " + final_yards);
         }
@@ -47,12 +38,10 @@ namespace myApp
             Double fnl_yrds;
             Double feet_sum = first_feet + scd_feet;
             if (feet_sum < 5280) { miles = 0; }
-            else { miles = Math.Truncate(feet_sum/5280); }
-                
+            else { miles = Math.Truncate(feet_sum/5280); }     
             fnl_yrds = (feet_sum%5280)/3;
             fnl_feet = (feet_sum%5280)%3; 
-            
-             Console.WriteLine("Miles: " + miles);
+            Console.WriteLine("Miles: " + miles);
             Console.WriteLine("Yards: " + Math.Truncate(fnl_yrds));
             Console.WriteLine("Feet : " + fnl_feet);
         }
@@ -65,14 +54,9 @@ namespace myApp
             Double seconddist;
             String val;
             Double firstdist;
-
-
-
             // Prompt user for miles yards or feet 
             Console.WriteLine("Enter (miles = m) (feet = f) or ( yards = y)");
             String x1 = Console.ReadLine();
-            
-            
             if (x1 == "m"){
                 Console.WriteLine("Enter 1st Distance in Miles:");
                  val = Console.ReadLine();
@@ -91,7 +75,6 @@ namespace myApp
                  firstdist = Convert.ToDouble(val);
                 Input_First_Dist.Add(firstdist);
             }
-            
             // Getting Second Distance 
             Console.WriteLine(" Now enter (miles = m) (feet = f) or ( yards = y)");
             String x2 = Console.ReadLine(); 
@@ -112,12 +95,10 @@ namespace myApp
                  val = Console.ReadLine();
                  seconddist = Convert.ToDouble(val);
                 Input_Scd_Dist.Add(seconddist);
-            }
-            
+            }        
                 // Determing which function to use - if any 
                 // Maybe come up with a loop for the follwing 
                 //////////////////////////////////////////////
-                
                 /// Miles is first choice 
             if ((x1 == "m") && (x2 =="m")){
                 final_miles = Input_First_Dist[0] + Input_Scd_Dist[0];
