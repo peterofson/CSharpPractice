6875b3598665890459f2d252c1c7bc35c4bd7446
David Peterson <44169089+peterofson@users.noreply.github.com>
2019-03-12 22:04:56 -0400

    Add files via upload

---
+++ DistanceCalc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DistanceCalc.Models;
+
+namespace DistanceCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(AdditionViewModel model, string command, string push, string calc)
+        {
+            if ((command == "Miles") && (push == "Miles") && (calc == "Calculate"))
+            {
+                model.Miles = model.A + model.B;
+                
+            }
+            if (command == "Yards")
+            {
+                model.Miles = model.A - model.B;
+            }
+            if (command == "Feet")
+            {
+                model.Miles = model.A * model.B;
+            }
+
+            return View(model);
+        }
+
+
+    }
+}
