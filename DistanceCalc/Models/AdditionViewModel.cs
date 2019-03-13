6bc7912736c1722210d1379d3205856367bc7d75
David Peterson <44169089+peterofson@users.noreply.github.com>
2019-03-12 22:06:10 -0400

    Add files via upload

---
+++ DistanceCalc/Models/AdditionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace DistanceCalc.Models
+{
+
+    public class AdditionViewModel {
+
+        public double A { get; set; }
+
+        public double B { get; set; }
+
+        public double Miles { get; set; }
+        public double Yards { get; set; }
+        public double Feet { get; set;  }
+
+        public double Calculates
+        {
+            get
+            {
+                return Miles;
+            }
+            set { Miles = value; }
+        }
+
+    }
+
+}
+
+
